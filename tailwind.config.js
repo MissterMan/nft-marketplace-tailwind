2d81a36f26272d8b68c00512085b73cb4694435b
MissterMan <putrayogi4545@gmail.com>
2022-02-01 11:45:21 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ['./public/**/*.{html,js}'],
+  theme: {
+    extend: {
+      backgroundSize:{
+        '6': '1.5rem',
+      },
+      fontFamily:{
+        'satoshi' : ['Satoshi-Variable', 'sans-serif']
+      },
+    },
+  },
+  plugins: [],
+}
