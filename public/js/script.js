9c8abee5d414c6ce66ff041349ecc3a8446870ff
MissterMan <putrayogi4545@gmail.com>
2022-02-09 20:34:33 +0700

    fixing responsive navbar

--- public/js/script.js
+++ public/js/script.js
@@ -1,5 +1,11 @@
-const dark = document.getElementById('btnDark');
-btnDark.addEventListener('click', function(){
-    const html = document.getElementsByTagName('html')[0];
-    html.classList.toggle('dark');
+const dark = document.getElementById("btnDark");
+btnDark.addEventListener("click", function () {
+  const html = document.getElementsByTagName("html")[0];
+  html.classList.toggle("dark");
+});
+
+const ham = document.querySelector("#hamburger");
+ham.addEventListener("click", function () {
+  const mobile = document.querySelector("#mobile");
+  mobile.classList.toggle("hidden");
 });
